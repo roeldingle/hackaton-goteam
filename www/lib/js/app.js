ff92e6d873fdebec7bd75cf6667f2ddd4d811ac6
Justin Rodrigo <jkrodrigo@straightarrow.com.ph>
2017-05-23 12:12:51 +0800

    new

--- www/lib/js/app.js
+++ www/lib/js/app.js
@@ -49,7 +49,7 @@ goteam.controller("SplitterController", function(){
 
 goteam.controller("myTeamController", function($scope, $http) {
 
-    $http.get('../test-api.php').
+    $http.get('http://straightarrowasset.com/test/api-test.php').
     success(function(data) {
       $scope.team = eval(data);
     }).
